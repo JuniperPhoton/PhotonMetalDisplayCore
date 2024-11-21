69918dc4e73cb0666295e793e2823ebaf190ec7a
dengweichao <dengweichao@hotmail.com>
2024-11-22 00:31:30 +0800

    Add script to build doc

---
+++ docs.sh
@@ -0,0 +1,13 @@
+# Run the xcodebuild command
+xcodebuild docbuild -scheme PhotonMetalDisplayCore \
+    -derivedDataPath ./.build/derived-data \
+    -destination 'generic/platform=iOS' \
+    DOCC_HOSTING_BASE_PATH='PhotonMetalDisplayCore'
+
+# Check if the command was successful
+if [ $? -eq 0 ]; then
+    # Find and copy the .doccarchive to the current directory, renaming it to 'docs'
+    find ./.build/derived-data -type d -name 'PhotonMetalDisplayCore.doccarchive' -exec cp -R {} ./docs \;
+else
+    echo "xcodebuild command failed."
+fi

c578c9ce246d80c037a0ba1664b76d1957ab8bc7
dengweichao <dengweichao@hotmail.com>
2024-12-11 20:47:29 +0800

    Update docs

--- docs.sh
+++ docs.sh
@@ -6,6 +6,7 @@ xcodebuild docbuild -scheme PhotonMetalDisplayCore \
 
 # Check if the command was successful
 if [ $? -eq 0 ]; then
+    rm -rf ./docs
     # Find and copy the .doccarchive to the current directory, renaming it to 'docs'
     find ./.build/derived-data -type d -name 'PhotonMetalDisplayCore.doccarchive' -exec cp -R {} ./docs \;
 else

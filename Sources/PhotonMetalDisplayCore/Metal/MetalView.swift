14264effc3467dccff80277ef45332c29292e9f1
juniperphoton <dengweichao@hotmail.com>
2026-02-04 21:03:36 +0900

    Don't startMonitorConfig in the metal view

--- Sources/PhotonMetalDisplayCore/Metal/MetalView.swift
+++ Sources/PhotonMetalDisplayCore/Metal/MetalView.swift
@@ -157,7 +157,7 @@ public class CustomMTKView: MTKView {
     
     func setupObserver() {
 #if canImport(UIKit)
-        observer = HDRContentDisplayObserver(startMonitorConfig: .default) { [weak self] range in
+        observer = HDRContentDisplayObserver(startMonitorConfig: .none) { [weak self] range in
             guard let self else { return }
             setupLayerDynamicRange()
         }
